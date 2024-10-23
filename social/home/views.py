e89ee813ebb6aa2b445b5f9c8ab5952d7d784160
mehran-dgn <mehrandehghanan@yahoo.com>
2024-10-23 21:30:42 +0330

    post delete and updated added

--- social/home/views.py
+++ social/home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect 
 from django.views import View 
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .forms import PostUpdateForm
 # Create your views here.
 
 class HomeView(View):
@@ -16,3 +19,35 @@ class PostDetailView(View):
         post = Post.objects.get(pk=post_id , slug=post_slug)
         return render(request, 'home/detail.html',{'post':post})
     
+class PostDeleteView(LoginRequiredMixin,View):
+    def get(self,request,post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request , "Post deleted successfully" , 'success')
+        else:
+            messages.success(request,"You can't delete this post" , 'danger')
+
+        return redirect("home:home")
+
+class PostUpdateView(LoginRequiredMixin , View):
+    form_class = PostUpdateForm
+
+    def dispatch(self,request,*args,**kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        if not post.user.id == request.user.id :
+            messages.error(request,"You can't update this post" ,'danger')
+            return redirect("home:home")
+
+    def get(self,request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance = post)
+        return render(request,"home/update.html" , {'form':form})
+
+    def post(self,request , post_id ):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(request.POST , instance=post)
+        if form.is_valid():
+            form.save()
+            messages.succes(request, 'You Updated this post' , 'success')
+            return redirect("home:post_detail",post.id , post.slug)
